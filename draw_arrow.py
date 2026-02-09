d83735d6520bbec7f79757c492de68eb3cb01d27
HidemasaBessho <96453382+HidemasaBessho@users.noreply.github.com>
2026-02-09 11:18:54 +0900

    Update draw_arrow.py

--- draw_arrow.py
+++ draw_arrow.py
@@ -6,74 +6,61 @@ import math
 import matplotlib.cm as cm  # colormap
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.interpolate import CubicSpline
-from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 plt.rcParams["font.size"] = 20
 
 plt.rcParams['font.family'] = 'Arial' #使用するフォント名
 plt.rcParams["text.usetex"] =True
 plt.rc('text', usetex=True)
-# Axesを追加
-#plt.figure(1)
-fig = plt.figure(figsize=(13,8)) ##サイズ指定##
 
+fig = plt.figure(figsize=(12,8)) ##サイズ指定##
 ax1 = fig.add_subplot(111)
 
 plt.xscale('log')
-# plt.yscale('log')
+plt.yscale('log')
 
-#plt.scatter(1, 0.845798, c='k',label="all")
+gamma,txy,P,U,xi,dr,dr0,z=np.loadtxt("./dp1e-5/AQS/pm0.9/Shear_dp0.000010.dat", comments='!', unpack=True, skiprows=1)
+ax1.plot(gamma/1.e-5,txy/P/(1.e-5)**0.5,"-o",markersize=6,linewidth=3.0,color="purple",label=r"$\delta\varphi=10^{-5}$")
 
-dp,gamma,Us,Ue,fs,fe,Gs,Ge=np.loadtxt("./pm0.9_SS/dp1e-4/relax_slope_dp0.00010_range.dat", comments='!', unpack=True)
-ax1.errorbar(gamma,Us,Ue,ecolor="red",marker="o",markerfacecolor="red",color="red",capsize=7,linestyle="none",markersize=10,lw=3.5,zorder=8)
-ax1.errorbar(gamma,fs,fe,ecolor="blue",marker="^",markerfacecolor="blue",color="blue",capsize=7,linestyle="none",markersize=10,lw=3.5,zorder=9)
-ax1.errorbar(gamma,Gs,Ge,ecolor="green",marker="s",markerfacecolor="green",color="green",capsize=7,linestyle="none",markersize=10,lw=3.5,zorder=7)
+gamma,txy,P,U,xi,dr,dr0,z=np.loadtxt("./dp1e-4/AQS/pm0.9/Shear_dp0.000100.dat", comments='!', unpack=True, skiprows=1)
+ax1.plot(gamma/1.e-4,txy/P/(1.e-4)**0.5,"-^",markersize=6,linewidth=3.0,color="blue",label=r"$10^{-4}$")
 
+gamma,txy,P,U,xi,dr,dr0,z=np.loadtxt("./dp5e-4/Shear_dp0.000500.dat", comments='!', unpack=True, skiprows=1)
+ax1.plot(gamma/5.e-4,txy/P/(5.e-4)**0.5,"-s",markersize=6,linewidth=3.0,color="deepskyblue",label=r"$5\times10^{-4}$")
 
-ax1.axhline(y=0.5,lw=1,color="red",linestyle="-",alpha=0.7)
-ax1.axhline(y=0.84,lw=3,color="red",linestyle=":",alpha=0.7)
+gamma,txy,P,U,xi,dr,dr0,z=np.loadtxt("./dp1e-3/AQS/pm0.9/Shear_dp0.001000.dat", comments='!', unpack=True, skiprows=1)
+ax1.plot(gamma/1.e-3,txy/P/(1.e-3)**0.5,"-D",markersize=6,linewidth=3.0,color="green",label=r"$10^{-3}$")
 
-ax1.axhline(y=0.75,lw=1,color="blue",linestyle="-",alpha=0.7)
-ax1.axhline(y=0.92,lw=3,color="blue",linestyle=":",alpha=0.7)
+gamma,txy,P,U,xi,dr,dr0,z=np.loadtxt("./dp3e-3/Shear_dp0.003000.dat", comments='!', unpack=True, skiprows=1)
+ax1.plot(gamma/3.e-3,txy/P/(3.e-3)**0.5,"-p",markersize=6,linewidth=3.0,color="darkorange",label=r"$3\times10^{-3}$")
 
-ax1.text(1.1,0.49, r"$\beta_{E}=1/2$",size=30,color="red")
-ax1.text(1.1,0.83, r"$\beta_{E}=0.84$",size=30,color="red")
-
-ax1.text(1.1,0.74, r"$\beta_{E}=3/4$",size=30,color="blue")
-ax1.text(1.1,0.91, r"$\beta_{E}=0.92$",size=30,color="blue")
-
-
-ax1.text(1.3e-05,1.065, r"$\gamma_{\mathrm{s}}(10^{-4})$",size=30,color="darkorange")
-ax1.text(0.015,1.065, r"$\gamma_{\mathrm{y}}$",size=30,color="darkorange")
-
+gamma,txy,P,U,xi,dr,dr0,z=np.loadtxt("./dp1e-2/Shear_dp0.010000.dat", comments='!', unpack=True, skiprows=1)
+ax1.plot(gamma/1.e-2,txy/P/(1.e-2)**0.5,"-v",markersize=6,linewidth=3.0,color="red",label=r"$10^{-2}$")
 
 ####矢印の描き方#####
 #plt.annotate("",xy=(終点のx, 終点のy), xytext=(始点のx, 始点のy),arrowprops=dict(edgecolor=矢印の枠線の色,facecolor=矢印の塗りつぶしの色, shrink=0.05,linewidth=線の太さ))
 
-plt.annotate("",xy=(1.66936679363601e-05, 1.0), xytext=(1.66936679363601e-05, 1.05),arrowprops=dict(edgecolor='darkorange',facecolor='none', shrink=0.05,linewidth=0.5))
-plt.annotate("",xy=(0.0195954512783951, 1.0), xytext=(0.0195954512783951, 1.05),arrowprops=dict(edgecolor='darkorange',facecolor='darkorange', shrink=0.05,linewidth=0.5))
+plt.annotate("",xy=(1.66936679363601e-01, 1.e-7), xytext=(1.66936679363601e-01, 1.e-6),arrowprops=dict(edgecolor='black',facecolor='black', shrink=0.05,linewidth=0.5))
+ax1.text(3.e-2,2.e-6, r"$\gamma_{\mathrm{s}}/\delta\varphi$",size=40,color="black")
 
+plt.xlim(3.e-8,3.e+5)
+plt.ylim(1.e-7,1.e+2)
+plt.xticks(color='k', size=42)
+plt.yticks(color='k', size=42) #軸の書式
+plt.xlabel(r"$$\gamma/\delta\varphi$$",color='k', size=42)
+plt.ylabel(r"$\mu(\gamma)/\delta\varphi^{1/2}$",color='k', size=42) #軸ラベル
 
-plt.xticks(color='k', size=35)
-plt.yticks(color='k', size=35)
-plt.xlabel(r"$$\gamma$$",color='k', size=35)
-plt.ylabel(r"$\beta$",color='k', size=35)
 #目盛設定
-plt.xlim(9.e-8,1)
-plt.ylim(0.3,1)
-plt.tick_params(axis='x', which='major',width = 1, length = 10, direction='in',pad=12)
-plt.tick_params(axis='x',which='minor',width = 1, length = 5, direction='in')
-ax1.tick_params(axis='y', which='major', labelsize=35, width=1, length=10, direction='in',pad=12)
-ax1.tick_params(axis='y', which='minor', labelsize=35, width=1, length=5, direction='in')
-ax1.xaxis.set_ticks_position('both')
-ax1.tick_params(axis='x', which='both', top=True)
+plt.tick_params(which='major',width = 1.5, length = 10, direction="in",pad=10)
+plt.tick_params(which='minor',width = 1.5, length = 5, direction="in",pad=10)
 ax1.spines['top'].set_linewidth(2.5)
 ax1.spines['bottom'].set_linewidth(2.5)
 ax1.spines['left'].set_linewidth(2.5)
 ax1.spines['right'].set_linewidth(2.5)
-leg=plt.legend(ncol=3, loc=2, borderaxespad=0.35, fontsize=27,frameon=True,edgecolor="black",handletextpad=0.2,handlelength=1)
+leg=plt.legend(ncol=1, loc=4, borderaxespad=0.35, fontsize=35,frameon=True,edgecolor="black",handletextpad=0.2,handlelength=1) #凡例の書式 locで位置指定/Trueで囲みをつける, Falseで囲みなし
 leg.get_frame().set_linewidth(2.5)
+for handle in leg.legend_handles:
+    handle.set_markersize(6)
 
 
-plt.savefig('./pm0.9_SS/dp1e-4/Fig_relax_slope.pdf',bbox_inches="tight")
-plt.show()
+plt.savefig('./shear_mu_dp0.000100.pdf',bbox_inches="tight") #保存
+plt.show() #下にプロット結果を出力
